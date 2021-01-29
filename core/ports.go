0216010b37c90bcef9f1df176636213f5387d6d2
Pritam Singh <pkspritam16@gmail.com>
2021-01-29 12:05:17 +0530

    adding ports
    Signed-off-by: Pritam Singh <pkspritam16@gmail.com>

--- core/ports.go
+++ core/ports.go
@@ -20,5 +20,27 @@ type BookStorePort interface {
 // UserStorePort represents userprofile database, which will be implemented by user store adapter
 // and used by BookCore
 type UserStorePort interface {
-	Store(ctx context.Context,user )
+	Store(ctx context.Context, user model.User) errors.E
+	Get(ctx context.Context, username string) (*model.User, errors.E)
+	Update(ctx context.Context, username string, user model.User) errors.E
+	Delete(ctx context.Context, username string) errors.E
+	Query(ctx context.Context, query map[string]interface{}, pageNumber, perPage int) ([]model.User, errors.E)
+}
+
+// OAuthStore represents oauth database, which will be implemented by oauth store adapter
+// and used by oauth core
+type OAuthStore interface {
+	Store(ctx context.Context, token model.Token) errors.E
+	Get(ctx context.Context, tokenID string) (*model.Token, errors.E)
+	Delete(ctx context.Context, tokeID string) errors.E
+}
+
+// CommentStore represents comment database, which will be implemented by comment store adapter
+// and used by commentCore
+type CommentStore interface {
+	Store(ctx context.Context, comment model.Comment) errors.E
+	Get(ctx context.Context, commentID string) (*model.Comment, errors.E)
+	Update(ctx context.Context, commentID string, comment model.Comment) errors.E
+	Delete(ctx context.Context, comment string) errors.E
+	Query(ctx context.Context, query map[string]interface{}, pageNumber, perPage int) ([]model.Comment, errors.E)
 }
