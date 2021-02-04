445c31396e4b7172f3a70ca79ff4db9513f6cc38
Pritam Singh <pkspritam16@gmail.com>
2021-02-04 11:28:23 +0530

    adding status code
    Signed-off-by: Pritam Singh <pkspritam16@gmail.com>

---
+++ pkg/code/code.go
@@ -0,0 +1,14 @@
+package code
+
+// code package defines all internal code used by this project
+
+// Status is status code
+type Status uint8
+
+const (
+	// CodeNotFound called when a item is not present
+	CodeNotFound Status = iota + 1
+
+	// CodeInternal called for internal server error
+	CodeInternal
+)
